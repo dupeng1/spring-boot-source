ef5c9e2e02e871c08a8e856ce928a976dfbcc6e0
dupeng1 <1115378691@qq.com>
2024-01-26 23:14:37 +0800

    第一次上传

---
+++ spring-boot-mytest/src/main/java/com/mytest/Application.java
@@ -0,0 +1,19 @@
+package com.mytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author dupeng
+ * @version V1.0
+ * Copyright 2022 Kidswant Children Products Co., Ltd.
+ * @Title: Application
+ * @Description: TODO
+ * @date 2024/1/25 23:11
+ */
+@SpringBootApplication
+public class Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
